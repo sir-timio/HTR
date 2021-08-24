165bc14e17daeb3d1e5b1d951b66372ec5cd9c6c
Timur-Ionov <t-ionov@list.ru>
2021-08-24 23:27:02 +0300

    add request template pls do something with it

--- bot/Bot.py
+++ bot/Bot.py
@@ -1,6 +1,7 @@
 import asyncio
+import requests
 import logging
-
+import random
 from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.utils.emoji import emojize
@@ -17,17 +18,31 @@ from aiogram.types import ReplyKeyboardRemove, \
 logging.basicConfig(format=u'%(filename)s [ LINE:%(lineno)+3s ]#%(levelname)+8s [%(asctime)s]  %(message)s',
                     level=logging.INFO)
 
-EXAMPLES = [
-    'AgACAgIAAxkDAANpYSPbzQABKkMms9fy9qm8Fh-snFnaAAJItjEbXn8ZSaTr2gnjem6mAQADAgADeAADIAQ',
-    'AgACAgIAAxkDAANoYSPbzdCDjvo12ckN6SGH_rOAJuoAAke2MRtefxlJsUadeGc-6nYBAAMCAAN4AAMgBA',
-    'AgACAgIAAxkDAANnYSPbzbJMac4mUQ37r0YswUDR2nEAAka2MRtefxlJTs3Q58lAfwoBAAMCAANtAAMgBA',
-]
+samples = np.loadtxt('data/samples.txt', dtype=str, comments="#")
+num_samples = len(samples)
 
-
-
+LINK = 'https://api.telegram.org/bot<TOKEN>/getFile?file_id=<FILE_ID>'.replace('<TOKEN>', TOKEN)
+PATH = 'https://api.telegram.org/file/bot<TOKEN>/<FILE_PATH>'.replace('<TOKEN>', TOKEN)
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+error_process_messages = [
+    'что-то в глаз попало, узнай, что я умею \help'
+]
+
+prediction_process_messages = [
+    'мне кажется, это @',
+    'похоже на @',
+    'я вижу в этом @',
+    'кажется, это @',
+    # '@[:3]... @!',
+    '@, угадал?'
+]
+
+demo_process_messages = [
+    'ну, раз сам писать не хочешь, поищу у себя что-нибудь...\nнашел!',
+    'да уж, не у всех бумажка с ручкой под рукой\nдержи'
+]
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
@@ -35,9 +50,9 @@ async def process_start_command(message: types.Message):
         resize_keyboard=True, one_time_keyboard=True
     ).add(KeyboardButton('/help'))
 
-    await message.reply('Привет! Я бот, который любит читать. Умею только на русском, но быстро учусь новому ::'
+    await message.reply(text(emojize('Привет! Я бот, который любит читать. Умею только на русском, но быстро учусь новому :nerd:'
                         '\nИспользуй /help, '
-                        'чтобы узнать список доступных команд!', reply_markup=start_kb)
+                        'чтобы узнать список доступных команд!')), reply_markup=start_kb, parse_mode=ParseMode.MARKDOWN)
 
 
 @dp.message_handler(commands=['help'])
@@ -60,16 +75,30 @@ async def process_help_command(message: types.Message):
 
 @dp.message_handler(commands=['examples'])
 async def process_examples_command(msg: types.Message):
-    media = []
-    for photo_id in EXAMPLES:
+    i = random.randint(0, num_samples-6)
+    media = [InputMediaPhoto(samples[i], 'жду что-то похожее')]
+    for photo_id in samples[i+1:i+5]:
         media.append(InputMediaPhoto(photo_id))
+
     await bot.send_media_group(msg.from_user.id, media)
 
 @dp.message_handler(commands=['demo'])
 async def process_photo_command(msg: types.Message):
-    caption = 'предсказание'
-
-    await bot.send_photo(msg.from_user.id, EXAMPLES[0],
+    label = 'prediction'
+    i = random.randint(0, num_samples)
+    if i > int(num_samples * 0.7):
+        replica = demo_process_messages[i % len(demo_process_messages)]
+        await bot.send_message(msg.from_user.id, text=replica)
+    caption = prediction_process_messages[i % len(prediction_process_messages)]
+    caption = caption.replace('@', label)
+    img_hash = samples[i]
+    link = LINK.replace('<FILE_ID>', img_hash)
+    r = requests.get(url=link)
+    file_path = r.json()['result']['file_path']
+    file_path = PATH.replace('<FILE_PATH>', file_path)
+    # use file path to download image via PIL
+    #https://stackoverflow.com/questions/7391945/how-do-i-read-image-data-from-a-url-in-python
+    await bot.send_photo(msg.from_user.id, img_hash,
                          caption=emojize(caption))
 
 
@@ -81,9 +110,11 @@ async def process_info_command(msg: types.Message):
 
 @dp.message_handler(content_types=['photo'])
 async def echo_img(msg: types.Message):
+    label = 'предсказание'
+    i = random.randint(0, len(prediction_process_messages))
+    caption = prediction_process_messages[i].replace('@', label)
     file_id = msg.photo[-1]['file_id']
-
-    await bot.send_photo(msg.from_user.id, file_id, caption='caption')
+    await bot.send_photo(msg.from_user.id, file_id, caption=caption)
 
 
 @dp.message_handler(content_types=ContentType.ANY)
